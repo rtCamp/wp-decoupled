19ad9a60957ae0be4f0c74cba79a18a12ffac381
Imran Sayed <imranhsayed@gmail.com>
2019-06-04 15:41:48 +0530

    Add endpoint to get woocommerce products

--- server.js
+++ server.js
@@ -4,12 +4,35 @@ const express = require( 'express' );
 const port = 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next( { dev } );
-
 const handle = app.getRequestHandler();
+
+require('dotenv').config();
+
+const WooCommerceAPI = require( 'woocommerce-api' );
+const WooCommerce = new WooCommerceAPI({
+	url: process.env.SITE_URL,
+	consumerKey: process.env.WOO_CONSUMER_KEY,
+	consumerSecret: process.env.WOO_SECRET,
+	wpAPI: true,
+	version: 'wc/v3'
+});
+
+
 
 app.prepare()
 	.then( () => {
 		const server = express();
+
+		/**
+		 * Get All products
+		 */
+		server.get( '/getProducts', ( request, response ) => {
+
+			// Get All Products
+			WooCommerce.get('products', function(err, data, res) {
+				response.json( JSON.parse(res) );
+			});
+		} );
 
 		server.get( '*', ( req, res ) => {
 			return handle( req, res );
