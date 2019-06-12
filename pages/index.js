b97c1a626d6c1aa94783021fd960b6ebff773a47
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-06-12 14:17:15 +0530

    Add Hero and Categories component

--- pages/index.js
+++ pages/index.js
@@ -26,22 +26,63 @@ const PRODUCTS_QUERY = gql`query {
 					}
 				}`;
 
+const Hero = () => (
+	<div className="wp-block-cover alignfull hero text-center mb-5">
+		<div className="wp-block-cover__inner-container">
+			<h1 >Welcome</h1>
+				<p className="has-text-color">This is your homepage which is what most visitors will see when they first visit your shop.</p>
+				<p className="has-text-color">You can change this text by editing the “Welcome” page via the “Pages” menu in your dashboard.</p>
+		</div>
+	</div>
+);
+
+const Categories = () => {
+	return (
+		<>
+			<h2 className="text-center mb-4">Shop by Category</h2>
+			<div className="woocommerce container">
+				<ul className="products row mx-auto">
+				<li className="product-category product first col-md-4">
+					<Link as={`/`} href={`/`}><a className="">
+						<img src="https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/accessories.jpg" alt="Accessories" width="324" height="324" srcSet="https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/accessories.jpg 801w, https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/accessories-150x150.jpg 150w, https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/accessories-300x300.jpg 300w, https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/accessories-768x768.jpg 768w" sizes="(max-width: 324px) 100vw, 324px" />		
+						<h2 className="woocommerce-loop-category__title">
+							Accessories <mark className="count">(4)</mark>		
+						</h2>
+					</a></Link>
+				</li>
+				<li className="product-category product col-md-4">
+					<Link as={`/`} href={`/`}><a className="">
+						<img src="https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/hoodies.jpg" alt="Hoodies" width="324" height="324" srcSet="https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/hoodies.jpg 800w, https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/hoodies-150x150.jpg 150w, https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/hoodies-300x300.jpg 300w, https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/hoodies-768x768.jpg 768w" sizes="(max-width: 324px) 100vw, 324px" />		
+						<h2 className="woocommerce-loop-category__title">
+							Hoodies <mark className="count">(4)</mark>		
+						</h2>
+					</a></Link>
+					</li>
+				<li className="product-category product last col-md-4">
+					<Link as={`/`} href={`/`}><a className="">
+						<img src="https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/tshirts.jpg" alt="Tshirts" width="324" height="324" srcSet="https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/tshirts.jpg 801w, https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/tshirts-150x150.jpg 150w, https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/tshirts-300x300.jpg 300w, https://woo-vsf.dev5.rt.gw/wp-content/uploads/2019/05/tshirts-768x768.jpg 768w" sizes="(max-width: 324px) 100vw, 324px" />		
+						<h2 className="woocommerce-loop-category__title">
+							Tshirts <mark className="count">(4)</mark>		
+						</h2>
+						</a></Link>
+					</li>
+				</ul>
+			</div>
+		</>
+	);
+}
+
 const Index = ( props ) => {
 
 	const { products } = props;
 
 	return (
 		<Layout>
-			<div className="wp-block-cover alignfull hero">
-				<div className="wp-block-cover__inner-container">
-					<h1 >Welcome</h1>
-						<p className="has-text-color">This is your homepage which is what most visitors will see when they first visit your shop.</p>
-						<p className="has-text-color">You can change this text by editing the “Welcome” page via the “Pages” menu in your dashboard.</p>
-				</div>
-			</div>
+			<Hero />
+			<Categories />
 			{ products.length ? (
 
-				<div>
+				<div className="mt-2">
 					<h3 className="text-center">Products</h3>
 					<div className="products-wrapper">
 						{
