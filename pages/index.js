5c216047bf3c0d85600b7255f1afcdc843b09946
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-06-13 12:33:57 +0530

    Hero working

--- pages/index.js
+++ pages/index.js
@@ -30,8 +30,8 @@ const Hero = () => (
 	<div className="wp-block-cover alignfull hero text-center mb-5">
 		<div className="wp-block-cover__inner-container">
 			<h1 >Welcome</h1>
-				<p className="has-text-color">This is your homepage which is what most visitors will see when they first visit your shop.</p>
-				<p className="has-text-color">You can change this text by editing the “Welcome” page via the “Pages” menu in your dashboard.</p>
+				<p className="has-text-color">Welcome to the WP Decoupled Demo page.</p>
+				<p className="has-text-color">This is a frontend for a WooCommerce Store created with Next.js and WPGraphQL.</p>
 		</div>
 	</div>
 );
