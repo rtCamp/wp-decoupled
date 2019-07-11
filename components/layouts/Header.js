524c117bba5458d2f1a33ad55f67662a51bf86b3
Muhammad Muhsin <muhammad.muhseen@gmail.com>
2019-07-11 13:55:26 +0530

    Add NProgress to show loading indicator

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -1,22 +1,33 @@
 import Link from 'next/link';
+import Router from "next/router";
+import NProgress from "nprogress";
 import CartIcon from "../cart/CartIcon";
 
+Router.onRouteChangeStart = () => {
+	NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+	NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+	NProgress.done();
+};
+
 const Header = () => (
-	<div className="wd-navbar-wrap">
-		<nav className="wd-navbar navbar navbar-expand-lg navbar-dark bg-primary">
-			<Link href="/"><a className="navbar-brand">WP Decoupled</a></Link>
-			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
-			        aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-			</button>
+	<nav className="wd-navbar navbar navbar-expand-lg navbar-dark bg-primary">
+		<Link href="/"><a className="navbar-brand">WP Decoupled</a></Link>
+		<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01"
+			aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+		</button>
 
-			<div className="collapse navbar-collapse" id="navbarColor01">
+		<div className="collapse navbar-collapse" id="navbarColor01">
 
-			</div>
-			<div className="cart">
-				<CartIcon/>
-			</div>
-		</nav>
-	</div>
+		</div>
+		<div className="cart">
+			<CartIcon />
+		</div>
+	</nav>
 );
 
 export default Header;
