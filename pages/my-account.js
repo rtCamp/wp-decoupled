5d73d55636948d73867b4027432e9c3254d9b7ac
Imran Sayed <imranhsayed@gmail.com>
2019-09-28 19:22:05 +0530

    Update login mutation query to get user nicename

--- pages/my-account.js
+++ pages/my-account.js
@@ -1,10 +1,35 @@
 import Layout from "../components/layouts/Layout";
+import { useState, useEffect } from 'react';
+import { isUserValidated } from "../utils/auth-functions";
+import isEmpty from "../validator/isEmpty";
 
 const MyAccount = () => {
 
+	const [ showContent, setShowContent ] = useState( false );
+	const [ userData, setUserData ] = useState( '' );
+
+	useEffect( () => {
+		const userValidatedData = isUserValidated();
+
+		if ( ! isEmpty( userValidatedData )  ) {
+
+			setUserData( userValidatedData );
+			setShowContent( true )
+		}
+
+	}, [] );
+	
+	console.warn( userData );
+
 	return (
 		<Layout>
-			<div>My Account</div>
+			{/* Only Show Content if user is logged in */}
+			{ showContent ? (
+				<div className="container mt-5">
+					<h4>My Account</h4>
+
+				</div>
+			) : ''}
 		</Layout>
 	)
 };
