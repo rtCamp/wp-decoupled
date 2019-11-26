4c8ebd818bf1ce956d43f87d2778eedd6b0d13de
Imran Sayed <imranhsayed@gmail.com>
2019-11-26 11:46:25 +0530

    Update readme

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,17 @@ import gql from 'graphql-tag';
 import AddToCartButton from "../components/cart/AddToCartButton";
 import Hero from "../components/home/Hero";
 
+
+const SimpleProductFragment = gql`
+   fragment SimplePr on SimpleProduct {
+        price
+      }
+`;
+
 /**
  * GraphQL products query.
  */
-const PRODUCTS_QUERY = gql`query {
+const PRODUCTS_QUERY = gql`query GET_ALL_PRODUCT {
 					products(first: 50) {
 						nodes {
 							id
@@ -23,18 +30,50 @@ const PRODUCTS_QUERY = gql`query {
 								sourceUrl
 							}
 							name
-							... on SimpleProduct {
-							price
-							}
-							... on VariableProduct {
-							price
-							}
-							... on ExternalProduct {
-							price
-							}
+							...SimplePr
 						}
 					}
-				}`;
+					
+				}
+				${SimpleProductFragment}
+				`;
+
+// const NewProducts = ({ products }) => {
+//
+// 	console.warn( products );
+//
+// 	return (
+// 		<div className="container mt-5">
+// 			<h2 className="text-center mb-5">Products</h2>
+// 			{ products.length ? (
+// 				<div className="mt-2">
+// 					<div className="products-wrapper row">
+// 						{
+// 							products.map( item => {
+// 								if ( item.price ) {
+// 									return (
+// 										<div className="product-container col-md-3 mb-5" key={item.id}>
+// 											<Link as={`/product/${item.slug}-${item.productId}`} href={`/product?slug=${item.slug}-${item.productId}`}>
+// 												<a>
+// 											<span className="product-link">
+// 												<img className="product-image" src={item.image.sourceUrl} srcSet={item.image.srcSet} alt={ item.name }/>
+// 												<h5 className="product-name">{item.name}</h5>
+// 												<p className="product-price">{item.price}</p>
+// 											</span>
+// 												</a>
+// 											</Link>
+// 											<AddToCartButton product={ item } />
+// 										</div>
+// 									)
+// 								}
+// 							} )
+// 						}
+// 					</div>
+// 				</div>
+// 			) : '' }
+// 		</div>
+// 	);
+// };
 
 const NewProducts = ({ products }) => {
 
@@ -48,7 +87,6 @@ const NewProducts = ({ products }) => {
 					<div className="products-wrapper row">
 						{
 							products.map( item => {
-								if ( item.price ) {
 									return (
 										<div className="product-container col-md-3 mb-5" key={item.id}>
 											<Link as={`/product/${item.slug}-${item.productId}`} href={`/product?slug=${item.slug}-${item.productId}`}>
@@ -56,14 +94,13 @@ const NewProducts = ({ products }) => {
 											<span className="product-link">
 												<img className="product-image" src={item.image.sourceUrl} srcSet={item.image.srcSet} alt={ item.name }/>
 												<h5 className="product-name">{item.name}</h5>
-												<p className="product-price">{item.price}</p>
+												<p className="product-price">$30</p>
 											</span>
 												</a>
 											</Link>
 											<AddToCartButton product={ item } />
 										</div>
 									)
-								}
 							} )
 						}
 					</div>
