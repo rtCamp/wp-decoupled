1057fa2b7c9b08b3756ae177a9b79c5756a1b3ea
Vipin Kumar Dinkar <vipinkumard365@gmail.com>
2021-01-18 10:50:43 +0530

    add menu query

---
+++ src/queries/get-menus.js
@@ -0,0 +1,36 @@
+import { gql } from '@apollo/client';
+import MenuFragment from './fragments/menus';
+
+export const GET_MENUS = gql`
+    query GET_MENUS {
+        headerMenus: menuItems(where: { location: PRIMARY, parentId: "0" }) {
+            edges {
+                node {
+                    ...MenuFragment
+                    childItems {
+                        edges {
+                            node {
+                                ...MenuFragment
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        footerMenus: menuItems(where: { location: FOOTER, parentId: "0" }) {
+            edges {
+                node {
+                    ...MenuFragment
+                    childItems {
+                        edges {
+                            node {
+                                ...MenuFragment
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    ${MenuFragment}
+`;
