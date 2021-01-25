613048782e70d916fa544c10e4b474a0e2fc481e
Vijayaraghavan M <mvijayaraghavan29@gmail.com>
2021-01-25 11:45:55 +0530

    Fetch only required fields from GraphQL

--- src/queries/get-product-slug.js
+++ src/queries/get-product-slug.js
@@ -1,15 +1,11 @@
 import { gql } from '@apollo/client';
-import ProductFragment from './fragments/product';
 
 export default gql`
     query GET_PRODUCT_SLUGS {
         products: products {
-            edges {
-                node {
-                    ...ProductFragment
-                }
+            nodes {
+              slug
             }
         }
     }
-    ${ProductFragment}
 `;
