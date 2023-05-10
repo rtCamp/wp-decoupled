bca5d4c256c46fa1acb6ece674ec2185b020a544
thelovekesh <lovekesh.kumar@rtcamp.com>
2023-05-10 21:15:18 +0530

    Add eslint-plugin-prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 const eslintConfig = {
-    root: true,
-    extends: [
-        "next/core-web-vitals",
-        "prettier",
-    ]
+  root: true,
+  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
 };
 
 module.exports = eslintConfig;
